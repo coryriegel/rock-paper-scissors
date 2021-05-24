662188f36b0c427f37696149515152b887de372c
Cory Riegel <coryriegel98@gmail.com>
2021-05-24 14:38:28 -0400

    Updated to 5-round game

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-let choices = [`ROCK`,`PAPER`,`SCISSORS`];
-let computerSelection = choices[Math.floor(Math.random()*choices.length)];
-console.log(computerSelection)
-let playerSelection = prompt(`ROCK, PAPER, OR SCISSORS?`);
-playerSelection = playerSelection.toUpperCase();
-console.log(playerSelection)
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
 
-function playRound() {
-    if(playerSelection == `ROCK` && computerSelection == `PAPER`) {
-        alert(`You lost!`);
+    for(let i = 0; playerWins < 5 && computerWins < 5; i++) {
+        playRound();
     }
-    else if(playerSelection == `ROCK` && computerSelection == `SCISSORS`) {
-        alert(`You won!`);
+    function playRound() {
+        let choices = [`ROCK`,`PAPER`,`SCISSORS`];
+        let computerSelection = choices[Math.floor(Math.random()*choices.length)];
+        console.log(computerSelection)
+        let playerSelection = prompt(`ROCK, PAPER, OR SCISSORS?`);
+        playerSelection = playerSelection.toUpperCase();
+        console.log(playerSelection)
+
+        if(playerSelection == `ROCK` && computerSelection == `PAPER`) {
+            ++computerWins;
+            alert(`You lost! Rock loses to paper!\nYou: ${playerWins}\nComputer: ${computerWins}`);
+        }
+        else if(playerSelection == `ROCK` && computerSelection == `SCISSORS`) {
+            ++playerWins;
+            alert(`You won! Rock beats scissors!\nYou: ${playerWins}\nComputer: ${computerWins}`);
+        }   
+        else if(playerSelection == `SCISSORS` && computerSelection == `ROCK`) {
+            ++computerWins;
+            alert(`You lost! Scissors lose to rock!\nYou: ${playerWins}\nComputer: ${computerWins}`);
+        }
+        else if(playerSelection == `SCISSORS` && computerSelection == `PAPER`) {
+            ++playerWins;
+            alert(`You won! Scissors beat paper!\nYou: ${playerWins}\nComputer: ${computerWins}`);
+        }
+        else if(playerSelection == `PAPER` && computerSelection == `SCISSORS`) {
+            ++computerWins;
+            alert(`You lost! Paper loses to scissors!\nYou: ${playerWins}\nComputer: ${computerWins}`);
+        }
+        else if(playerSelection == `PAPER` && computerSelection == `ROCK`) {
+            ++playerWins;
+            alert(`You won! Paper beats rock!\nYou: ${playerWins}\nComputer: ${computerWins}`);
+        }
+        else if(playerSelection === computerSelection) {
+            alert(`It's a tie! Do-over!\nYou: ${playerWins}\nComputer: ${computerWins}`);
+        }
     }
-    else if(playerSelection == `SCISSORS` && computerSelection == `ROCK`) {
-        alert(`You lost!`);
+    
+    if(playerWins == 5) {
+        alert(`Congratulations, you won the game!`);
+        return `Thanks for playing!`;
     }
-    else if(playerSelection == `SCISSORS` && computerSelection == `PAPER`) {
-        alert(`You won!`);
-    }
-    else if(playerSelection == `PAPER` && computerSelection == `SCISSORS`) {
-        alert(`You lost!`);
-    }
-    else if(playerSelection == `PAPER` && computerSelection == `ROCK`) {
-        alert(`You won!`);
-    }
-    else if(playerSelection === computerSelection) {
-        alert(`It's a tie!`);
+    else if(computerWins == 5) {
+        alert(`Oh no! You lost the game!`);
+        return `Better luck next time!`;
     }
 }
-console.log(playRound())
+alert(game())
